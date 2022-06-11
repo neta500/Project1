877965dad84fba384aa2500e3dc30696aa1b8a15
shshin <talesrune@naver.com>
2022-06-11 23:03:35 +0900

    sessionmanager update

---
+++ ServerCore/SessionManager.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <set>
+
+#include "Session.h"
+#include "SpinLock.h"
+
+class SessionManager
+{
+public:
+	SessionManager(const IoContext& ioContext, const int sessionCount)
+		: _poolCount(sessionCount)
+	{
+		_sessionList.reserve(sessionCount);
+
+		for (int i = 0; i < sessionCount; ++i)
+		{
+			_sessionList.emplace_back(std::make_shared<Session>(ioContext));
+		}
+	}
+
+	std::shared_ptr<Session> AcquireSession()
+	{
+		WRITE_LOCK;
+
+		++_popIndex;
+		const int index = _popIndex % _poolCount;
+		const auto& session = _sessionList[index];
+		_sessionList[index] = nullptr;
+
+		_aliveSessionList.emplace(session);
+		return session;
+	}
+
+	void ReleaseSession(const std::shared_ptr<Session>& session)
+	{
+		WRITE_LOCK;
+
+		++_pushIndex;
+		const int index = _pushIndex % _poolCount;
+		_sessionList[index] = session;
+		_aliveSessionList.erase(session);
+	}
+
+	USE_LOCK;
+
+	const int _poolCount;
+	int _pushIndex = 0;
+	int _popIndex = 0;
+
+	std::vector<std::shared_ptr<Session>> _sessionList;
+	std::set<std::shared_ptr<Session>> _aliveSessionList;
+};
